526597d278d27eb76ee9393ee260e71b0722be53
Xoxa2244 <gorinra24@gmail.com>
2025-10-28 17:26:57 +0100

    Fix TypeScript error: update Player interface isWaitlist field to allow null in PlayerSelectionModal

--- components/PlayerSelectionModal.tsx
+++ components/PlayerSelectionModal.tsx
@@ -24,7 +24,7 @@ interface Player {
   dupr: string | null
   duprRating: string | null
   gender: string | null
-  isWaitlist: boolean
+  isWaitlist: boolean | null
   teamId?: string | null
   teamName?: string | null
 }
