521c204300e79a2047a3f24849870a89acb55d50
Xoxa2244 <gorinra24@gmail.com>
2025-10-28 17:16:11 +0100

    Fix TypeScript error: update Player interface isPaid and isWaitlist fields to allow null in EditPlayerModal

--- components/EditPlayerModal.tsx
+++ components/EditPlayerModal.tsx
@@ -14,8 +14,8 @@ interface Player {
   email: string | null
   dupr: string | null
   duprRating: string | null  // Decimal from Prisma serializes as string
-  isPaid: boolean
-  isWaitlist: boolean
+  isPaid: boolean | null
+  isWaitlist: boolean | null
 }
 
 interface EditPlayerModalProps {
