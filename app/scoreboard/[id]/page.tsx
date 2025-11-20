32f06028b77edac16b21f30d52c57386e9ce1072
ds136 <ds@piqle.io>
2025-11-20 14:59:22 +0300

    profile error

--- app/scoreboard/[id]/page.tsx
+++ app/scoreboard/[id]/page.tsx
@@ -248,7 +248,7 @@ export default function PublicCoursePage() {
                   {hasValidAvatar && !avatarError && avatarSrc ? (
                     <Image
                       src={avatarSrc}
-                      alt={session.user.name || 'Profile'}
+                      alt={session?.user?.name || 'Profile'}
                       width={32}
                       height={32}
                       className="rounded-full object-cover"
