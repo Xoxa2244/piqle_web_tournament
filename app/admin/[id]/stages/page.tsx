593ae4b9d02ae46a0db16bda6a3c847fc87861e8
Xoxa2244 <gorinra24@gmail.com>
2026-01-05 16:06:42 +0100

    Add missing closing brace for JSX expression in ternary operator

--- app/admin/[id]/stages/page.tsx
+++ app/admin/[id]/stages/page.tsx
@@ -2369,6 +2369,7 @@ function DivisionStageManagementContent() {
           </>
         )}
       </div>
+    }
 
       {/* Score input modal */}
       {!isIndyLeague && showScoreModal && selectedMatch && (() => {
