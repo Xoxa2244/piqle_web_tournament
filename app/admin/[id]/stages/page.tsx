f84c41aea55561b02401d6e9851ea0c24dc0e41a
Xoxa2244 <gorinra24@gmail.com>
2026-01-05 18:30:55 +0100

    Add missing closing brace for JSX expression

--- app/admin/[id]/stages/page.tsx
+++ app/admin/[id]/stages/page.tsx
@@ -2375,6 +2375,7 @@ function DivisionStageManagementContent() {
           </>
         )}
       </div>
+    }
 
       {/* Score input modal */}
       {!isIndyLeague && showScoreModal && selectedMatch && (() => {
