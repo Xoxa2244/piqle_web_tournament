c7058d3b4a3a84a44f1ef7ad1b7dc9722426da98
ds136 <ds@piqle.io>
2026-01-28 15:27:27 +0300

    error fix

--- app/api/complaint/route.ts
+++ app/api/complaint/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
-import nodemailer from 'nodemailer'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
+import * as nodemailer from 'nodemailer'
 
 export async function POST(req: NextRequest) {
   try {
